440ce5470e50d6382469e728a759f6e62e0291db
larryworm1127 <shilerong@gmail.com>
2018-07-09 17:30:57 +0800

    insert proper icon when clicked on the cell

--- board_games/static/tic_tac_toe/script.js
+++ board_games/static/tic_tac_toe/script.js
@@ -1,6 +1,33 @@
 var game = {
     user: '',
-    computer: ''
+    computer: '',
+    currentPlayer: ''
 };
 
+function setFig(id) {
+    if (id === 'start-computer') {
+        game.computer = '<i id="cross" class="fas fa-times"></i>';
+        game.user = '<i id="circle" class="far fa-circle"></i>';
+        game.currentPlayer = 'computer'
+    } else if (id === 'start-human') {
+        game.user = '<i id="cross" class="fas fa-times"></i>';
+        game.computer = '<i id="circle" class="far fa-circle"></i>';
+        game.currentPlayer = 'user'
+    }
+}
+
+function draw(id) {
+    let cell = $('#' + id);
+
+    if (game.currentPlayer === 'user') {
+        cell.html(game.user);
+        cell.removeAttr('onclick');
+        game.currentPlayer = 'computer';
+
+    } else if (game.currentPlayer === 'computer') {
+        cell.html(game.computer);
+        cell.removeAttr('onclick');
+        game.currentPlayer = 'user';
+    }
+}
 
