682b73988cbff218b1767fd86d1825b4c1e33dd4
larryworm1127 <shilerong@gmail.com>
2018-07-09 11:45:26 +0800

    finished tic tac toe front-end

--- board_games/static/script.js
+++ board_games/static/script.js
@@ -3,18 +3,4 @@ var game = {
     computer: ''
 };
 
-function start() {
-    $('#startModal').modal('show');
-}
 
-function setFig(id) {
-    if (id === 'x') {
-        game.user = '<span class="fa fa-times"></span>';
-        game.computer = '<span class="fa fa-circle-o"></span>';
-    } else if (id === 'o') {
-        game.user = '<span class="fa fa-circle-o"></span>';
-        game.computer = '<span class="fa fa-times"></span>';
-    }
-}
-
-setTimeout(start, 600);
--- board_games/views/games.py
+++ board_games/views/games.py
@@ -5,7 +5,7 @@ Flask "games" blueprint
 @author: Larry Shi
 """
 
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, render_template, jsonify)
 
 game = Blueprint('board_games', __name__, url_prefix='/board_games')
 
@@ -18,3 +18,13 @@ def index():
 @game.route('/tic_tac_toe')
 def tic_tac_toe():
     return render_template('games/tic_tac_toe.html')
+
+
+@game.route('/2048')
+def twenty_forty_eight():
+    return render_template('games/2048.html')
+
+
+@game.route('/blackjack')
+def blackjack():
+    return render_template('games/blackjack.html')
