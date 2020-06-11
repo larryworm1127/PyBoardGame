0a2a468640e56807c02fe3bf0f036bac24ea122c
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-06-11 14:25:51 -0400

    updated config

--- board_games/__init__.py
+++ board_games/__init__.py
@@ -9,57 +9,46 @@ Flask app factory
 import os
 
 from flask import Flask, render_template
+from dotenv import load_dotenv
 
 # Constant
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Create and configure the app
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
-
-# Load the instance config, if it exists
-app.config.from_pyfile('config.py', silent=True)
-
-# Ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+# Load environment variables
+load_dotenv()
 
 
-# Register individual apps
-@app.route('/')
-def index():
-    return render_template('index.html')
+def create_app():
+    flask_app = Flask(__name__)
+    flask_app.config.from_object(os.environ['APP_SETTINGS'])
+
+    # Register individual apps
+    @flask_app.route('/')
+    def index():
+        return render_template('index.html')
+
+    # Register blueprints
+    from .views import twenty_forty_eight
+    flask_app.register_blueprint(twenty_forty_eight.bp)
+    flask_app.add_url_rule('/', endpoint='index')
+
+    from .views import tic_tac_toe
+    flask_app.register_blueprint(tic_tac_toe.bp)
+    flask_app.add_url_rule('/', endpoint='index')
+
+    from .views import blackjack
+    flask_app.register_blueprint(blackjack.bp)
+    flask_app.add_url_rule('/', endpoint='index')
+
+    from .views import sudoku
+    flask_app.register_blueprint(sudoku.bp)
+    flask_app.add_url_rule('/', endpoint='index')
+
+    from .views import about
+    flask_app.register_blueprint(about.bp)
+    flask_app.add_url_rule('/', endpoint='index')
+
+    return flask_app
 
 
-# Register blueprints
-# 2048 game blueprint
-from .views import twenty_forty_eight
-
-app.register_blueprint(twenty_forty_eight.bp)
-app.add_url_rule('/', endpoint='index')
-
-# tic-tac-toe game blueprint
-from .views import tic_tac_toe
-
-app.register_blueprint(tic_tac_toe.bp)
-app.add_url_rule('/', endpoint='index')
-
-# blackjack game blueprint
-from .views import blackjack
-
-app.register_blueprint(blackjack.bp)
-app.add_url_rule('/', endpoint='index')
-
-# sudoku game blueprint
-from .views import sudoku
-
-app.register_blueprint(sudoku.bp)
-app.add_url_rule('/', endpoint='index')
-
-# about pages blueprint
-from .views import about
-
-app.register_blueprint(about.bp)
-app.add_url_rule('/', endpoint='index')
+app = create_app()
--- config.py
+++ config.py
@@ -4,7 +4,31 @@ Production/Deployment config module for the Flask app
 @date: 7/31/2018
 @author: Larry Shi
 """
+import os
 
-# config variables
-SECRET_KEY = b'c47da79190345efef83858ae4596dbaa4e04f7fc888a6f34'
-DEBUG = False
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
